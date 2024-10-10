d4c8135beacfc9293f27a8a1bc26fcd100483321
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-10-10 16:07:58 -0300

    :test_tube: test: adiciona verificação para renderização da rota de Cartões

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom'
 import App from './paginas/Principal/App'
+import Cartoes from './componentes/Cartoes'
 
 describe('Rotas', () => {
     test('Deve renderizar a rota principal', () => {
@@ -9,5 +10,22 @@ describe('Rotas', () => {
         const usuario = screen.getByText('Olá, Joana :)!')
 
         expect(usuario).toBeInTheDocument()
+    })
+
+    test('Deve renderizar a rota Cartões', () => {
+        const rota = '/cartoes'
+        render(
+            <MemoryRouter initialEntries={[rota]}>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route path="cartoes" element={<Cartoes />} />
+                    </Route>
+                </Routes>
+            </MemoryRouter>
+        )
+
+        const meusCartoes = screen.getByText('Meus cartões')
+
+        expect(meusCartoes).toHaveTextContent('Meus cartões')
     })
 })
