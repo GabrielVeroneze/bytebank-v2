409c6db68ed02f8597cf95d87ce889cb84db5321
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-10-12 23:46:59 -0300

    :test_tube: test: adiciona teste para hooks com renderHook

---
+++ src/hooks/hooks.test.js
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react'
+import { useEffect, useState } from 'react'
+
+test('Hooks', () => {
+    const { result } = renderHook(() => {
+        const [nome, setNome] = useState('')
+
+        useEffect(() => {
+            setNome('Alice')
+        }, [])
+
+        return nome
+    })
+
+    expect(result.current).toBe('Alice')
+})
